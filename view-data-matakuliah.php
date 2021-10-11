0086126bcfeeef40ff2125f6b83b77161e97d4f8
nur cahya <90689628+nurcahyaaa10@users.noreply.github.com>
2021-10-11 11:49:26 +0700

    Add files via upload

---
+++ view-data-matakuliah.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tampil Data Matakuliah</title>
+</head>
+<body>
+    <center>
+        <table>
+            <tr>
+                <th colspan="3">
+                    Tampil Data Mata Kuliah</th></tr>
+                <tr><td colspan="3"><hr></td></tr>
+            <tr>
+            <th>Kode MTK   :</th><th><?= $kode; ?></th>
+            </tr>
+            <tr>
+                <th>Nama MTK  :</th><th><?= $nama; ?></th>
+            </tr>
+            <tr>
+            <th>SKS               :</th><th><?= $sks; ?></th>
+            </tr>
+            <tr>
+                <td colspan="3" align="center">
+                    <a href="<?= base_url('matakuliah'); ?>">Kembali</a>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
